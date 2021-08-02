4b7fd6ee80db506453a4f684e27db43eb4dc7a1f
Jethro Castillo <jethrocastlewebdev@gmail.com>
2021-08-01 22:01:27 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import 'bootstrap';
+import 'bootswatch/dist/cosmo/bootstrap.min.css'; // Added this :boom:
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeadersComponent } from './shared/components/headers/headers.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
