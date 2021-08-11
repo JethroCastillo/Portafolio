e32741cb7b60d6d48e597457fe877f9e13638d58
Jethro Castillo <jethrocastlewebdev@gmail.com>
2021-08-10 21:48:22 -0500

    Added some animations

--- src/app/components/pages/about/about.module.ts
+++ src/app/components/pages/about/about.module.ts
@@ -4,16 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { LeafsComponent } from 'src/app/shared/animations/leafs/leafs.component';
 
 
 @NgModule({
   declarations: [
-    AboutComponent
-  ],
+    AboutComponent,
+    LeafsComponent,
+    ],
   imports: [
     CommonModule,
     AboutRoutingModule,
-    NgxUsefulSwiperModule
+    NgxUsefulSwiperModule,
+    
   ]
 })
 export class AboutModule { }
---
+++ src/app/shared/animations/leafs/leafs.component.ts
@@ -0,0 +1,61 @@
+import { ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leafs',
+  templateUrl: './leafs.component.html',
+  styleUrls: ['./leafs.component.scss']
+})
+export class LeafsComponent implements OnInit {
+
+  imgleafs = ['https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/13255/31347/leaf__40593.1560276423.png?c=2', "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Leaf_icon_15.svg/1003px-Leaf_icon_15.svg.png", "https://pic.onlinewebfonts.com/svg/img_498940.png", "https://www.pinclipart.com/picdir/big/120-1209055_leaf-icons-download-for-free-in-png-and.png"];
+
+  constructor(private el:ElementRef, private render: Renderer2) { }
+  
+  ngOnInit(): void {  
+    var container =document.getElementById('leafcontainer');
+    for(let i=0; i<6; i++){
+      container?.appendChild(this.createLeaf());
+    }
+  }
+  
+  randomVal(min:number, max:number){
+    const rand = Math.floor(Math.random() * (max - min +1) + min)
+    return rand;
+  }
+
+  randomArr(array:any) {
+    const arritem = Math.floor(Math.random() * array.length)
+    const item = array[arritem]
+    return item;
+  }
+
+  initLeaf(){
+    var container =document.getElementById('leafcontainer');
+    return container?.appendChild(this.createLeaf());
+  }
+
+  createLeaf(){
+    // Create the elements
+    var leafDiv = this.render.createElement('div');
+    var image = this.render.createElement('img');
+
+    // Get Random Item 
+    image.src = this.randomArr(this.imgleafs);
+
+    // Leaf div Animations 
+    leafDiv.className = 'leafs';
+    leafDiv.style.left = this.randomVal(100,700) +'px';
+    leafDiv.style.animationName = 'drop, fade';
+    leafDiv.style.animationDuration = this.randomVal(3,12) +'s';
+
+    // leaf img Animations
+    image.style.animationName = 'spinAndFlip';
+    image.style.animationDuration = this.randomVal(3,9)+'s';
+
+    leafDiv.appendChild(image);
+
+    return leafDiv;
+  }
+
+}
